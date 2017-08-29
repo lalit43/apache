c935706bc03b1348436848ff46f74f6af3760ea2
lalit43 <lalit43@hotmail.com.com>
2017-08-29 04:48:49 +0000

    initail commit

---
+++ recipes/server.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: apache
+# Recipe:: server
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, World!</h>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+
